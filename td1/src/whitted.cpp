17e77bf32685dabb5fcb96e9489c7f7034f3118f
Bastien Soucasse <bastien.soucasse@icloud.com>
2022-01-24 08:24:46 +0100

    Implement whitted

--- td1/src/whitted.cpp
+++ td1/src/whitted.cpp
@@ -1,11 +1,18 @@
 #include "integrator.h"
 #include "scene.h"
 
-class DirectIntegrator : public Integrator {
-public:
-    DirectIntegrator(const PropertyList& props) { }
+class WhittedIntegrator : public Integrator
+{
+private:
+    const int m_maxRecursion;
 
-    Color3f Li(const Scene* scene, const Ray& ray) const {
+public:
+    WhittedIntegrator(const PropertyList &props) : m_maxRecursion{props.getInteger("maxRecursion", 0)}
+    {
+    }
+
+    Color3f Li(const Scene *scene, const Ray &ray) const
+    {
         Hit hit;
 
         scene->intersect(ray, hit);
@@ -15,10 +22,10 @@ public:
 
         Color3f color = Color3f(0.f);
 
-        // const Point3f intersection_point = ray.origin + ray.direction * hit.t();
         const Point3f intersection_point = ray.at(hit.t());
 
-        for (const Light* light : scene->lightList()) {
+        for (const Light *light : scene->lightList())
+        {
             const Ray lightRay(intersection_point + hit.normal() * 10e-4, light->direction(intersection_point).normalized());
             Hit lightHit;
 
@@ -30,13 +37,21 @@ public:
             color += rho * std::max(light->direction(intersection_point).dot(hit.normal()), 0.f) * light->intensity(intersection_point);
         }
 
+        if (ray.recursionLevel < m_maxRecursion)
+        {
+            const Vector3f r = 2 * hit.normal().dot(-ray.direction) * hit.normal() + ray.direction;
+            Ray newRay(intersection_point + hit.normal() * 10e-4, r.normalized());
+            newRay.recursionLevel = ray.recursionLevel + 1;
+            color += Li(scene, newRay) * std::max(r.dot(hit.normal()), 0.f) * hit.shape()->material()->reflectivity();
+        }
+
         return color;
     }
 
     std::string toString() const
     {
-        return "DirectIntegrator[]";
+        return "WhittedIntegrator[]";
     }
 };
 
-REGISTER_CLASS(DirectIntegrator, "direct")
+REGISTER_CLASS(WhittedIntegrator, "whitted");
